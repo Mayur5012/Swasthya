e17ee8e4c359636cb0c02fe214ad33b56b2f4946
Mayuresh Gawali <mayureshgawali9@gmail.com>
2024-10-12 14:01:33 +0530

    production

--- index.js
+++ index.js
@@ -1,50 +1,44 @@
-const bodyParser = require("body-parser");
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+
 const PlasmaRoutes = require("./routes/Plasma");
 const hospitalRoutes = require("./routes/hospital");
 const medsRoutes = require("./routes/meds");
 const oxygenRoutes = require("./routes/oxygen");
 const ambulanceRoutes = require("./routes/ambulance");
 const volunteerRoutes = require("./routes/volunteer");
-const cors = require("cors");
 
+const app = express();
 const PORT = 5000;
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("backend is connected", {
+mongoose
+  .connect(process.env.MONGO_URL_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  });
-});
-
-mongoose
-  .connect(process.env.MONGO_URL_CONNECTION)
+  })
   .then(() => {
-    console.log("mongodb is connected");
+    console.log("MongoDB is connected");
   })
   .catch((err) => {
-    console.log(err);
+    console.log("MongoDB connection error:", err);
   });
 
-/* middleware */
+
 app.use(cors({
-  origin:["http://localhost:3000", "https://swasthya-serivce.onrender.com"]
+  origin: ["http://localhost:3000", "https://swasthya-serivce.onrender.com"]
 }));
 app.use(morgan("dev"));
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-/* routes */
+
 app.use("/api", PlasmaRoutes);
 app.use("/api", hospitalRoutes);
 app.use("/api", medsRoutes);
@@ -52,6 +46,12 @@ app.use("/api", oxygenRoutes);
 app.use("/api", ambulanceRoutes);
 app.use("/api", volunteerRoutes);
 
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log("server is listening at port 5000");
+  console.log(`Server is listening at port ${PORT}`);
 });
